2fd5c8b132b3482c7e6818a204696d0c7c2f78fb
klm <kylymaratov@gmail.com>
2024-10-13 20:19:49 +0600

    compile first version androidtv

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.example.tvapp"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
+        versionCode = 1
         versionName = "1.0"
     }
 
